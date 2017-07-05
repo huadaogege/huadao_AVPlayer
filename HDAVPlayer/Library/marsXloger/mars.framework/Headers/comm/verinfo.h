78d99fdd9fc4b463bf2212aa5d9941f30af65460
cyg <cuiyuguan@zhizhangyi.com>
2017-07-05 09:56:33 +0800

    add mars.framework

---
+++ HDAVPlayer/Library/marsXloger/mars.framework/Headers/comm/verinfo.h
@@ -0,0 +1,11 @@
+
+#ifndef Mars_verinfo_h
+#define Mars_verinfo_h
+
+#define MARS_REVISION "4494a50"
+#define MARS_PATH "master"
+#define MARS_URL ""
+#define MARS_BUILD_TIME "2017-04-01 15:04:50"
+#define MARS_TAG "huadao_[2017-04-01_15.04]@git-4494a50@cuiyuguan"
+
+#endif
