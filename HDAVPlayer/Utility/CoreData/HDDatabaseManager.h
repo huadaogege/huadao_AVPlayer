9b6a3e65f557618ff1e785cd080b1d720bf5c015
cyg <cuiyuguan@zhizhangyi.com>
2017-07-05 18:54:02 +0800

    solve conflict

--- HDAVPlayer/Utility/CoreData/HDDatabaseManager.h
+++ HDAVPlayer/Utility/CoreData/HDDatabaseManager.h
@@ -3,20 +3,12 @@
 //  HDAVPlayer
 //
 //  Created by huadao on 2017/7/5.
-<<<<<<< HEAD
-//  Copyright © 2017年 崔玉冠. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-
-=======
 //  Copyright © 2017年 huadao. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import "HDFileBasicInfo+CoreDataClass.h"
 #import "HDFileBasicInfoModel.h"
->>>>>>> origin/develop-cyg
 
 @interface HDDatabaseManager : NSObject
 
@@ -24,14 +16,8 @@
 
 + (HDDatabaseManager *)shareInstance;
 
-<<<<<<< HEAD
-//- (HDFileInfo *)insertFileBasicInfoIntoSource:(HDFileBasicInfoModel*)model tableName:(NSString *)tableName;
-//
-//- (NSArray *)objectsFromSourceWithTableName:(NSString *)tableName;
-=======
 - (HDFileBasicInfo *)insertFileBasicInfoIntoSource:(HDFileBasicInfoModel*)model tableName:(NSString *)tableName;
 
 - (NSArray *)objectsFromSourceWithTableName:(NSString *)tableName;
->>>>>>> origin/develop-cyg
 
 @end
--- HDAVPlayer/Utility/CoreData/HDDatabaseManager.m
+++ HDAVPlayer/Utility/CoreData/HDDatabaseManager.m
@@ -3,11 +3,7 @@
 //  HDAVPlayer
 //
 //  Created by huadao on 2017/7/5.
-<<<<<<< HEAD
-//  Copyright © 2017年 崔玉冠. All rights reserved.
-=======
 //  Copyright © 2017年 huadao. All rights reserved.
->>>>>>> origin/develop-cyg
 //
 
 #import <UIKit/UIKit.h>
@@ -32,38 +28,6 @@
     return _managedObjectContext;
 }
 
-<<<<<<< HEAD
-//- (NSArray *)objectsFromSourceWithTableName:(NSString *)tableName {
-//    NSEntityDescription *entityDescription = [NSEntityDescription entityForName:tableName inManagedObjectContext:self.managedObjectContext];
-//    NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:tableName];
-//    [fetchRequest setEntity:entityDescription];
-//    NSError *error;
-//    NSArray *array = [self.managedObjectContext executeFetchRequest:fetchRequest error:&error];
-//    if (error) {
-//        NSLog(@"%@", error);
-//    }
-//    return array;
-//}
-//
-//- (HDFileInfo *)insertFileBasicInfoIntoSource:(HDFileBasicInfoModel*)model tableName:(NSString *)tableName {
-//    NSArray *insertedArray = [self objectsFromSourceWithTableName:tableName];
-//    for (HDFileInfo *info in insertedArray) {
-//        if ([info.filePath isEqualToString:model.filePath]) {
-//            return info;
-//        }
-//    }
-//    HDFileInfo *fileBasicInfo = (HDFileInfo *)[NSEntityDescription insertNewObjectForEntityForName:tableName inManagedObjectContext:self.managedObjectContext];
-//    fileBasicInfo.fileName = model.fileName;
-//    fileBasicInfo.filePath = model.filePath;
-//    fileBasicInfo.isRead = model.isRead;
-//    fileBasicInfo.lastProgress = model.lastProgress;
-//    NSError *error;
-//    if ([self.managedObjectContext save:&error]) {
-//        NSLog(@"保存成功！");
-//    }
-//    return fileBasicInfo;
-//}
-=======
 - (NSArray *)objectsFromSourceWithTableName:(NSString *)tableName {
     NSEntityDescription *entityDescription = [NSEntityDescription entityForName:tableName inManagedObjectContext:self.managedObjectContext];
     NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:tableName];
@@ -94,7 +58,6 @@
     }
     return fileBasicInfo;
 }
->>>>>>> origin/develop-cyg
 
 
 @end
